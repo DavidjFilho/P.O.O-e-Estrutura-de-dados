285df4617159e373546a92c7b267b9c096a246b4
DavidjFilho <105315629+DavidjFilho@users.noreply.github.com>
2022-08-24 19:53:12 -0300

    28082022

---
+++ Exercicio01/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int A = 15;
+        int B = -7;
+        int X = A + B;
+
+        System.out.println(X);
+
+    }
+}
+
---
+++ exercicio08/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int numeroFuncionario = input.nextInt();
+        int quantidadeHrsTrab = input.nextInt();
+        double recebeHora = input.nextDouble();
+        double salario = quantidadeHrsTrab * recebeHora;
+
+        System.out.println("NUMBER = " + numeroFuncionario);
+        System.out.printf("SALARIO = U$ %.2f%n", salario);
+
+
+
+
+        input.close();
+
+    }
+}
